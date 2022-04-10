7c5bbc66ca4e9a9eced4e0e2e5c64a037006faa7
wendemsky <61985456+wendemsky@users.noreply.github.com>
2022-04-10 23:48:02 +0530

    Added solution - LeetHub

---
+++ 6. Count Smaller Than X - GFG/6.-count-smaller-than-x.c
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include<stdio.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+// arr[]: input array
+// n: size of array
+// x: element for which you need to find smaller than x
+int smallerThanX(int arr[], int n, int x)
+{
+    // your code here
+    int small=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]<x) small++;
+    }
+    return small;
+}
+
+// { Driver Code Starts.
+
+int main() {
+	int T;
+	scanf("%d", &T);
+	while(T--) 
+	{
+	    int N;
+	    scanf("%d", &N);
+	    int arr[N];
+	    for(int i=0;i<N;i++)
+	        scanf("%d", &arr[i]);
+	    int x;
+	    scanf("%d", &x);
+	    printf("%d", smallerThanX(arr,N,x));
+	    printf("\n");
+	}
+	return 0;
+}  // } Driver Code Ends
