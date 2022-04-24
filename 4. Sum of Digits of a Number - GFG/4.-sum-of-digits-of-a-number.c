0d1697e2332a1f049915a0060148b07a45c4fbff
wendemsky <61985456+wendemsky@users.noreply.github.com>
2022-04-24 23:12:48 +0530

    Added solution - LeetHub

---
+++ 4. Sum of Digits of a Number - GFG/4.-sum-of-digits-of-a-number.c
@@ -0,0 +1,37 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+int sumOfDigits(int n)
+{
+    if(n==0) return 0;
+    return n%10 + sumOfDigits(n/10);
+}
+
+// { Driver Code Starts.
+
+int main() {
+	int T;
+	
+	//taking testcases
+	scanf("%d", &T);
+	while(T--)
+	{
+	    int n;
+	    
+	    //taking input n
+	    scanf("%d", &n);
+	    
+	    //calling method sumOfDigits()
+	    
+	    printf("%d\n", sumOfDigits(n));
+	    
+	    
+	}
+	return 0;
+}  // } Driver Code Ends
