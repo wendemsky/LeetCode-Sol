d3b38238581f6403ede49e8166ba7fc4a941b2de
wendemsky <61985456+wendemsky@users.noreply.github.com>
2022-05-24 21:47:10 +0530

    Added solution - LeetHub

--- 16. Is Array Sorted - GFG/16.-is-array-sorted.cpp
+++ 16. Is Array Sorted - GFG/16.-is-array-sorted.cpp
@@ -14,7 +14,7 @@ class Solution{
     //Complete this function
     int isSorted(int arr[],int n)
     {
-     if(arr[0]>arr[1]){
+     if(arr[0]>arr[n-1]){
          for(int i=1;i<n-1;i++){
              if(arr[i]<arr[i+1]) return 0;
          }
