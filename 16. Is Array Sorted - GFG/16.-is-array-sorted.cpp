32e7551649c5e60035a27a3230c8480fb7006953
wendemsky <61985456+wendemsky@users.noreply.github.com>
2022-05-24 21:46:15 +0530

    Added solution - LeetHub

---
+++ 16. Is Array Sorted - GFG/16.-is-array-sorted.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include<iostream>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    //Complete this function
+    int isSorted(int arr[],int n)
+    {
+     if(arr[0]>arr[1]){
+         for(int i=1;i<n-1;i++){
+             if(arr[i]<arr[i+1]) return 0;
+         }
+     }else{
+         for(int i=1;i<n-1;i++){
+             if(arr[i]>arr[i+1]) return 0;
+         }
+     }
+     return 1;
+    }
+};
+
+// { Driver Code Starts.
+
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--) 
+	{
+	    int N;
+	    cin>>N;
+	    int arr[N];
+	    for(int i=0;i<N;i++)
+	    cin>>arr[i];
+	    Solution obj;
+	    cout<<obj.isSorted(arr,N);
+	    cout<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
