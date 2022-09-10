1723608c6af9562e8dc6f6db38cfd1416cba7dbe
Himanshu Maithani <61985456+wendemsky@users.noreply.github.com>
2022-09-10 07:53:50 +0530

    Time: 0 ms (100.00%), Space: 6 MB (22.98%) - LeetHub

--- 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
@@ -10,13 +10,13 @@
 class Solution {
 public:
     int guessNumber(int n) {
-        long long l = 1, r = n, mid = (l+r)/2;
+        int l = 1, r = n, mid = l + (r-l)/2;
         while(l<=r){
             if(guess(mid) == 0) return mid;
             if(guess(mid) == -1) r = mid-1;
             else l = mid+1;
             
-            mid = (l+r)/2;
+            mid = l + (r-l)/2;
         }
         
         return -1;
