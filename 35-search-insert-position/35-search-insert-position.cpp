fef11adfc7d58a368fdb91ee3e1f6b58f7f1d56c
Himanshu Maithani <61985456+wendemsky@users.noreply.github.com>
2022-09-11 11:25:06 +0530

    Time: 16 ms (7.25%), Space: 9.7 MB (22.91%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -4,10 +4,8 @@ public:
         int beg=0,end=nums.size()-1,mid=(beg+end)/2;
         while(beg<=end){
             if(nums[mid]==target) return mid;
-            else{
-                if(nums[mid]<target) beg=mid+1;
-                else end=mid-1;
-            }
+            if(nums[mid]<target) beg=mid+1;
+            else end=mid-1;
             mid=(beg+end)/2;
         }
         return beg;
