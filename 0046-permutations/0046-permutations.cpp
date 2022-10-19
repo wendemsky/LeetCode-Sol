7e4c43a249c914de801aefa5f81bb233c6e25f2e
Himanshu Maithani <61985456+wendemsky@users.noreply.github.com>
2022-10-19 21:43:44 +0530

    Time: 5 ms (56.35%), Space: 7.7 MB (56.06%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    void f(vector<int> &arr, vector<int> &ds, int freq[], vector<vector<int>> &ans){
-        if(ds.size()==arr.size()){
-            ans.push_back(ds);
+    void f(int ind, vector<int> &arr, vector<vector<int>> &ans){
+        if(ind==arr.size()){
+            ans.push_back(arr);
             return;
         }
-        for(int i=0; i<arr.size();i++){
-            if(!freq[i]){
-                ds.push_back(arr[i]);
-                freq[i]=1;
-                f(arr, ds, freq, ans);
-                freq[i]=0;
-                ds.pop_back();
-            }
+        for(int i=ind; i<arr.size();i++){
+            swap(arr[ind],arr[i]);
+            f(ind+1, arr, ans);
+            swap(arr[i], arr[ind]);
         }
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        int freq[nums.size()] ;
-        for(int i=0;i<nums.size();i++) freq[i]=0;
-        f(nums, ds, freq, ans);
-        
+        f(0, nums, ans);
         return ans;
     }
 };
