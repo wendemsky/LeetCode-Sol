d7de5bc2d6968fbf2bc8eab218dd4e1506621eac
Himanshu Maithani <61985456+wendemsky@users.noreply.github.com>
2022-10-26 11:57:19 +0530

    Added solution - LeetHub

---
+++ Sum of distinct elements - GFG/sum-of-distinct-elements.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Find the sum of all non-repeated elements
+	// in an array
+	int findSum(int arr[], int n) {
+	    // code here
+	    sort(arr, arr+n);
+	    int sum = 0;
+	    for(int i=0;i<n;i++){
+	        if(arr[i]!=arr[i+1]){
+	            sum+=arr[i];
+	        }
+	    }
+	    return sum;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.findSum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
