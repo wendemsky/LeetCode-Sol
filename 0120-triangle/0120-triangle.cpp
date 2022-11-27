0818511294009d89716a2b2d286416eb5fc587b8
Himanshu Maithani <61985456+wendemsky@users.noreply.github.com>
2022-11-27 11:13:31 +0530

    Time: 19 ms (17.91%), Space: 8.9 MB (52.70%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -13,18 +13,22 @@ public:
     
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        vector<int> front(n, 0);
         for(int j=0; j<n; j++){
-            dp[n-1][j] = triangle[n-1][j];
+            front[j] = triangle[n-1][j];
         }
         
+        //Filled the last row, start from second last row
         for(int i=n-2; i>=0 ; i--){
+            //Each row has i col's
+            vector<int> curr(n, 0);
             for(int j=i; j>=0; j--){
-                int d = triangle[i][j] + dp[i+1][j];
-                int dg = triangle[i][j] + dp[i+1][j+1];
-                dp[i][j] = min(d, dg);
+                int d = triangle[i][j] + front[j];
+                int dg = triangle[i][j] + front[j+1];
+                curr[j] = min(d, dg);
             }
+            front = curr;
         }
-        return dp[0][0];
+        return front[0];
     }
 };
