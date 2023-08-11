1eba17a79cdc2caca83742f0935aa4f0fc4a4929
Himanshu Maithani <61985456+wendemsky@users.noreply.github.com>
2023-08-11 18:47:33 +0530

    Time: 3 ms (98.3%), Space: 7.4 MB (13.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        unordered_map<char, int> mpp;
+        for(int i=0;i<s.size();i++) mpp[s[i]]++;
+        for(int i=0;i<t.size();i++) mpp[t[i]]--;
 
-        return s==t;
+        for(auto e: mpp){
+            if(e.second!=0) return false;
+        }
+        return true;
     }
 };
